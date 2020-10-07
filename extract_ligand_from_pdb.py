a522f99a3c5a634a05ce78b285512eed7bc6c82e
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-10-07 18:21:08 -0300

    Add files via upload

---
+++ extract_ligand_from_pdb.py
@@ -0,0 +1,59 @@
+import argparse
+from collections import defaultdict
+
+
+def PDB_ID(input_file):
+    """file with PDB entry list"""
+    pdb_ids = open(input_file)
+    lines=pdb_ids.readlines()
+    strip_pdb_ids = [line.strip() for line in lines ]
+
+    pdb_ids.close()
+    return strip_pdb_ids
+
+def pdb_ligands_mapping(input_file):
+    """ mapping_ligands_pdb
+    columns: 0=PDB_ID; 1=ligands"""
+    mapping = open(input_file)
+    mapping_lines=mapping.readlines()
+    mapping.close()
+    dic_out={}
+    for line in mapping_lines:
+        lines = line.split(',')
+        key = lines[0].strip()
+        value = lines[1].strip()
+        if key in dic_out:
+          dic_out[key].append(value)
+        else:
+          dic_out[key] = [value]
+    return dic_out
+
+def id_cross(PDB_data, mapping_data):
+    """Cross IDs between inputs file"""
+
+    for pdb in PDB_data:
+        if pdb in mapping_data:
+            for ligand in mapping_data[pdb]:
+                print(pdb+"\t"+str(ligand))
+    return 0
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description='Extract ligands from PDB entries')
+    parser.add_argument("-p", '--PDB_list', default=None, help="Input file should have PDB ID list")
+    parser.add_argument("-m", '--mapping_ligands_pdb',  default="lig_pairs.txt", help="Input file of ligands and PDB ID link:http://www.ebi.ac.uk/thornton-srv/databases/pdbsum/data/lig_pairs.lst")
+
+    return parser
+
+def main():
+
+    parser=parse_arguments()
+    args=parser.parse_args()
+
+    id_cross(PDB_ID(args.PDB_list), pdb_ligands_mapping(args.mapping_ligands_pdb))
+    return 0
+
+
+if __name__=='__main__':
+    main()
