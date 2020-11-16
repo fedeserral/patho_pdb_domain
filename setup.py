98f493c280ef2be6d282203994c143527f625192
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-11-16 15:26:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 setup(name='patho_pdb_domain',
 		version='0.4.1',
-		py_modules=['uniprot'],
-		scripts=['uniprot'],
+		py_modules=['MOAD_PDBBIN','extracts','ligand_from_pdb'],
+		scripts=['MOAD_PDBBIN/MOAD.py','MOAD_PDBBIN/filter_MOAD.py', 'MOAD_PDBBIN/toMolar.py', 'MOAD_PDBBIN/PDBBIND.py', 'extracts/extract_ligand_from_pdb.py'
+			 ,'extracts/extract_pdb_from_domain.py','extracts/protein_id_extract_to_uniprot.py'],
 
 		requires=['requests','argparse'],
 
-		author='Jan Rudolph',
+		author='Federico Serral',
 		license='MIT license',
-		author_email='jan.daniel.rudolph@gmail.com',
-		description='Simple interface for uniprot.org',
-		url='https://github.com/jdrudolph/uniprot',
-		long_description=open('README').read(),
+		author_email='fedeserral92@gmail.com',
+		description='Extract true ligands',
+		url='https://github.com/fedeserral/patho_pdb_domain',
+		long_description='',
 		)
