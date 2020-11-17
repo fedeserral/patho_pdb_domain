02e99db1d9a02d5d2a52534527087fc6570b41bb
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-11-17 15:32:34 -0300

    Update request_ligand_from_PDBe.py

--- ligand_from_pfam/request_ligand_from_PDBe.py
+++ ligand_from_pfam/request_ligand_from_PDBe.py
@@ -1,6 +1,36 @@
 import requests
 import json
 
+def pdb_ligand_data(ligands):
+   #
+   r = requests.post("https://www.ebi.ac.uk/pdbe/api/pdb/compound/summary/",data=",".join(ligands))
+   if r.ok:
+       data =  r.json()  
+       new_data = []
+       for k,v in data.items():
+           r = v[0]            
+           r2 = {"smiles":r["smiles"][0]["name"],"pdb_ligand":k}
+           if "chembl_id" in r:
+               r2["chembl_id"] = r["chembl_id"]            
+           new_data.append(r2)
+       
+       return new_data
+   raise Exception(r.text)
+
+def search_chembl(chembl_id):
+   r = requests.get(f"https://www.ebi.ac.uk/unichem/rest/src_compound_id_all/{chembl_id}/1")
+   if r.ok:
+       data =  r.json()
+       for x in data:
+           x["src_name"] = sources[x["src_id"]]
+       return data
+   raise Exception(r.text)
+
+def pdb_ligand_data_batch(ligands,n=400):    
+   ligs_chunks = [ligs[i:i + n] for i in range(0, len(ligs), n)]
+   all_pdb_ligands = reduce(list.__add__, [pdb_ligand_data( chunk ) for chunk in tqdm(ligs_chunks)])    
+   return all_pdb_ligands
+
 def ligands_from_pdbs(pdbs):
     pdbs_count=len(pdbs)
     response_total={}
