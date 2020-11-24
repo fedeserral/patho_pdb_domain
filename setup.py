56dc0bf8d994a5aacb8cb193374aa29a65b5813a
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-11-24 13:18:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(name='patho_pdb_domain',
-		version='0.0.10',
+		version='0.0.11',
 		py_modules=['MOAD_PDBIND','extracts','ligand_from_pfam',
 			    'ligand_from_pfam.domain_pdb_ligand','ligand_from_pfam.request_ligand_from_PDBe',
-			    'extracts.extract_ligand_from_pdb.py','extracts.extract_pdb_from_domain.py','extracts.protein_id_extract_to_uniprot.py'],
+			    'extracts.extract_ligand_from_pdb','extracts.extract_pdb_from_domain','extracts.protein_id_extract_to_uniprot'],
 		scripts=['MOAD_PDBIND/MOAD.py','MOAD_PDBIND/filter_MOAD.py', 'MOAD_PDBIND/toMolar.py', 'MOAD_PDBIND/PDBBIND.py', 'extracts/extract_ligand_from_pdb.py'
 			 ,'extracts/extract_pdb_from_domain.py','extracts/protein_id_extract_to_uniprot.py'
 			 ,'ligand_from_pfam/domain_pdb_ligand.py','ligand_from_pfam/request_ligand_from_PDBe.py'],
