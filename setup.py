241d5b41921efd5e87562cd9e541cbf6b2412c95
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-11-25 14:24:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='patho_pdb_domain',
 		py_modules=['MOAD_PDBIND','extracts','ligand_from_pfam',
 			    'ligand_from_pfam.domain_pdb_ligand','ligand_from_pfam.request_ligand_from_PDBe',
 			    'extracts.extract_ligand_from_pdb','extracts.extract_pdb_from_domain',
-			    'extracts.protein_id_extract_to_uniprot', 'MOAD_PDBIND.filter_MOAD],
+			    'extracts.protein_id_extract_to_uniprot', 'MOAD_PDBIND.filter_MOAD'],
 		scripts=['MOAD_PDBIND/MOAD.py','MOAD_PDBIND/filter_MOAD.py', 'MOAD_PDBIND/toMolar.py', 'MOAD_PDBIND/PDBBIND.py', 'extracts/extract_ligand_from_pdb.py'
 			 ,'extracts/extract_pdb_from_domain.py','extracts/protein_id_extract_to_uniprot.py'
 			 ,'ligand_from_pfam/domain_pdb_ligand.py','ligand_from_pfam/request_ligand_from_PDBe.py'],
